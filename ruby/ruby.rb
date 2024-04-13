6ea1504cf64c9cd99f63e348bc83598594822e65
Dikendev <diken.dev@gmail.com>
2024-04-13 00:24:57 -0300

    string literal / string evaluation

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,3 +1,6 @@
 properties = ['object-oriented', 'duck typed', 'productive', 'fun']
 
 properties.each { | property | puts "Ruby is #{property}"}
+
+language = 'Ruby'
+puts "The language is #{language}"
