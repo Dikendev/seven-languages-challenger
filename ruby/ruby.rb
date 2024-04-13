72e22944391da4d7bb5e599f43cf0d404bd17217
Dikendev <diken.dev@gmail.com>
2024-04-13 12:59:42 -0300

    conditionals

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -16,3 +16,16 @@ end
 puts 'This appears to be true.' if not true
 
 puts 'This appears to be true.' if !true
+
+x = 0
+while x < 10
+  x = x + 1
+  puts x
+end
+
+puts 'This appears to be true if 1' if 1
+puts 'This appears to be true if random string ?' if 'random string'
+puts 'This appears to be true if 0' if 0
+puts 'This appears to be true if true' if true
+puts 'This appears to be true if false' if false
+puts 'This appears to be true if nil' if nil
