7f6d1a6c6d75ac66cae29b1faf936f46704db31e
Dikendev <diken.dev@gmail.com>
2024-04-13 20:32:26 -0300

    initial regex

--- ruby/day_1.rb
+++ ruby/day_1.rb
@@ -1,5 +1,4 @@
 sampleString = 'Hello'
-
 def substitute_first_vowel(string)
   string.sub(/[aeiou]/, '*')
 end
@@ -9,3 +8,18 @@ def substitute_all_vowels(string)
   string.gsub(/[aeiou]/, '*')
 end
 # H*ll*
+
+regexp = /red/
+regexp.match?('redirect') # true
+regexp.match?('credit') # true
+regexp.match?('bored') # true
+regexp.match?('foo') # false
+
+re = /foo/
+re.match('food') # foo
+re.match('good') # nil
+
+bar = /bar/ =~ 'foo bar' # 4
+
+'foo bar' =~ /bar/ # 4
+/baz/ =~ 'foo bar' # nil
