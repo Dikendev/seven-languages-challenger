b2b3c20375cbad303dae5c4e370c6981e58a6985
Dikendev <diken.dev@gmail.com>
2024-04-13 00:38:00 -0300

    some random methods

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,6 +1,8 @@
 properties = ['object-oriented', 'duck typed', 'productive', 'fun']
-
 properties.each { | property | puts "Ruby is #{property}"}
-
 language = 'Ruby'
 puts "The language is #{language}"
+puts 4.class
+sum = 4 + 4
+
+puts 4.even?
