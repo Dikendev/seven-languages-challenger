41539f353acce9c2b58284869334c45dca8c0a65
Dikendev <diken.dev@gmail.com>
2024-04-13 00:46:06 -0300

    decisions

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,8 +1,18 @@
-properties = ['object-oriented', 'duck typed', 'productive', 'fun']
-properties.each { | property | puts "Ruby is #{property}"}
-language = 'Ruby'
-puts "The language is #{language}"
-puts 4.class
-sum = 4 + 4
+x = 4 
 
-puts 4.even?
+puts 'This appear to be false' unless x == 4
+puts 'This appears to be true' if x == 4
+
+if x == 4
+  puts 'This appears to be true 1'
+end
+
+unless x == 4
+  puts 'This appears to be false'
+else 
+  puts 'This appears to be true else'
+end
+
+puts 'This appears to be true.' if not true
+
+puts 'This appears to be true.' if !true
