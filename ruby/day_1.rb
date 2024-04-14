bab4d24215539eb5299a59ccf8a8d9ab4d25b4fd
Dikendev <diken.dev@gmail.com>
2024-04-13 23:23:50 -0300

    guess game

--- ruby/day_1.rb
+++ ruby/day_1.rb
@@ -40,7 +40,26 @@ def print_name_times(string,number_times)
   number_times.times {puts string}
 end
 
-def print_text_times(number_times)
-  number_times.times { |index| puts "This is sentence number #{index + 1}"}
+def print_text_times(text ,number_times)
+  number_times.times { |index| puts "#{text} #{index + 1}"}
 end
-print_text_times(10)
+print_text_times('This is sentence number', 10)
+
+# gues the number game
+def guess_game(guess)
+  max_number = 10
+  random_number = rand(max_number)
+  tries = 0
+
+  while guess != random_number
+    puts 'Guess the number'
+    guess = gets.to_i
+    tries += 1
+    if guess < random_number
+      puts 'Too low'
+    elsif guess > random_number
+      puts 'Too high'
+    else puts "You got it, the number was #{random_number}, and it took you #{tries} tries"
+    end
+  end
+end
