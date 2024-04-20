01c106430f6be3828735fc600a8d4fc991901f5a
Dikendev <diken.dev@gmail.com>
2024-04-19 22:44:28 -0300

    some arrays lesson

---
+++ ruby/functions.rb
@@ -0,0 +1,17 @@
+animals = ['lions', 'tigers', 'bears']
+animals[1..animals.length]
+
+animals[1..animals.length].class
+(0..1).class
+
+animals[-2]
+animals[-1]
+animals[0]
+
+a = []
+a[0] = 'zero'
+a[1] = 1
+a[2] = ['two', 'things']
+
+print a
+
