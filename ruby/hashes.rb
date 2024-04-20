45cb6dc3220a3ce2a7bfa3acefa5c59d187c9f2a
Dikendev <diken.dev@gmail.com>
2024-04-19 22:56:45 -0300

    hashes

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,10 @@
-numbers = {1 => 'one', 2 => 'two', 3 => 'three'}
+numbersHashe = {1 => 'one', 2 => 'two', 3 => 'three'}
 
-p numbers
-p numbers[1]
+p numbersHashe
+p numbersHashe[1]
+p numbersHashe[2]
+
+stuff = {:statusCode => 201, :message => "Crated"}
+
+p stuff[:statusCode]
+p stuff[:message]
