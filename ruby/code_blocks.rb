49db18ccf562909285bce2e1668dbede2ebecf27
Dikendev <diken.dev@gmail.com>
2024-04-20 22:47:56 -0300

    more blocks

--- ruby/code_blocks.rb
+++ ruby/code_blocks.rb
@@ -14,5 +14,15 @@ class Integer
   end
 end
 # This is a bad practice called monkey patching. It is not recommended to do this.
+# 3.my_times {p 'hira there, kiddo'}
 
-3.my_times {p 'hira there, kiddo'}
+def call_block(&block)
+  block.call
+end
+
+def pass_block(&block)
+  call_block(&block)
+end
+
+pass_block {p 'hira there, kiddo'}
+# This is a good practice to pass a block to another method. It is recommended to do this.
