b93f2807e212426725f0877d28404a8d77128fca
Dikendev <diken.dev@gmail.com>
2024-04-20 22:45:23 -0300

    bad practice example

--- ruby/code_blocks.rb
+++ ruby/code_blocks.rb
@@ -1,1 +1,18 @@
-3.times {p 'hira there, kiddo'}
+# 3.times {p 'hira there, kiddo'}
+
+animals = ['lions', 'tigers', 'bears', 'oh my']
+# animals.each {|animal| p animal}
+
+
+class Integer
+  def my_times
+    i = self
+    while i > 0
+      i -= 1
+      yield
+    end
+  end
+end
+# This is a bad practice called monkey patching. It is not recommended to do this.
+
+3.my_times {p 'hira there, kiddo'}
